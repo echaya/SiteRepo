6d207a7766a9d1f80a50089b303c9d93ee8fddfa
Frederick Z. <echaya@gmail.com>
2024-12-15 14:11:54 +0800

    remove test (previous only tests)
    .

--- UpdateSite.bat
+++ UpdateSite.bat
@@ -9,6 +9,7 @@ ROBOCOPY %MoveFromDir% %MoveToDir% /S
 for /d /r . %%d in (.git) do if exist "%%d" if "%%~pd" NEQ "\Workspace\SiteRepo\" rd /s/q "%%d"
 for /d /r . %%d in (.github) do if exist "%%d" rd /s/q "%%d"
 for /d /r . %%d in (tests) do if exist "%%d" rd /s/q "%%d"
+for /d /r . %%d in (test) do if exist "%%d" rd /s/q "%%d"
 for /r "%MoveToDir%" %%f in (*.jpg, *.png, *.gif, *.bmp, *.tiff, *.svg) do del "%%f"
 
 del d:\Workspace\SiteRepo\site\pack\deps\opt\nvim-treesitter\parser-info\.gitignore
