e44bca6e6c804d1b1432e7931d5c325f0a922efd
Frederick Z. <echaya@gmail.com>
2025-01-05 14:19:21 +0800

    Plugin Update

--- site/pack/deps/opt/render-markdown.nvim/lua/render-markdown/integ/source.lua
+++ site/pack/deps/opt/render-markdown.nvim/lua/render-markdown/integ/source.lua
@@ -3,9 +3,9 @@ local state = require('render-markdown.state')
 local util = require('render-markdown.core.util')
 
 local list_markers = {
-    'list_marker_minus',
-    'list_marker_star',
-    'list_marker_plus',
+    list_marker_minus = '-',
+    list_marker_star = '*',
+    list_marker_plus = '+',
 }
 
 ---@class render.md.Source
@@ -31,21 +31,50 @@ function M.items(buf, row, col)
         return nil
     end
 
+    local children = { 'block_quote_marker', 'block_continuation' }
+    if vim.tbl_contains(children, node:type()) or list_markers[node:type()] ~= nil then
+        node = node:parent()
+        if node == nil then
+            return nil
+        end
+    end
+
     local items = {}
     local config = state.get(buf)
-    if vim.tbl_contains({ 'block_quote', 'block_quote_marker' }, node:type()) then
-        for _, component in pairs(config.callout) do
-            table.insert(items, M.item(component.raw, component.rendered, nil))
+    if node:type() == 'block_quote' then
+        local quote_row = node:range()
+        if quote_row == row then
+            for _, component in pairs(config.callout) do
+                table.insert(items, M.item(component.raw, component.rendered, nil))
+            end
         end
-    elseif node:type() == 'list_item' or vim.tbl_contains(list_markers, node:type()) then
+    elseif node:type() == 'list_item' then
         local checkbox = config.checkbox
-        table.insert(items, M.item('[ ]', checkbox.unchecked.icon, 'unchecked'))
-        table.insert(items, M.item('[x]', checkbox.checked.icon, 'checked'))
+        local prefix = M.list_prefix(row, node)
+        table.insert(items, M.item(prefix .. '[ ] ', checkbox.unchecked.icon, 'unchecked'))
+        table.insert(items, M.item(prefix .. '[x] ', checkbox.checked.icon, 'checked'))
         for name, component in pairs(checkbox.custom) do
-            table.insert(items, M.item(component.raw, component.rendered, name))
+            table.insert(items, M.item(prefix .. component.raw .. ' ', component.rendered, name))
         end
     end
     return items
+end
+
+---@private
+---@param row integer
+---@param node TSNode
+---@return string
+function M.list_prefix(row, node)
+    local marker_node = node:named_child(0)
+    if marker_node == nil then
+        return ''
+    end
+    local marker_row = marker_node:range()
+    if marker_row == row then
+        return ''
+    end
+    local marker = list_markers[marker_node:type()]
+    return marker ~= nil and marker .. ' ' or ''
 end
 
 ---@private
